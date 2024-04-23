be185d8810570b2e7f60f3c69df9a9308be6d920
Hogjesty <thinkabout228@gmail.com>
2024-04-23 20:59:32 +0300

    trying to prevent clipping

--- Assets/Scripts/Camera/CameraScript.cs
+++ Assets/Scripts/Camera/CameraScript.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
+using Cursor = UnityEngine.Cursor;
 
 public class CameraScript : MonoBehaviour {
     [SerializeField] private float verticalSensitivity;
@@ -9,19 +11,21 @@ public class CameraScript : MonoBehaviour {
     [SerializeField] private float lerpRatio;
     [SerializeField] private Transform playerTransform;
     [SerializeField] private LayerMask obstacle;
+
+    private Camera camera;
     
     private float cameraDistance;
+    private float actualCameraDistance;
 
     private float axisY;
     private float axisX;
 
     private void Start() {
-        axisX = 0;
-        axisY = 0;
-
+        camera = GetComponent<Camera>();
         Cursor.lockState = CursorLockMode.Locked;
+        actualCameraDistance = cameraDistance;
     }
-
+    
     private void LateUpdate() {
         axisX += Input.GetAxis("Mouse X") * Time.deltaTime * verticalSensitivity;
         axisY -= Input.GetAxis("Mouse Y") * Time.deltaTime * horizontalSensitivity;
@@ -29,27 +33,32 @@ public class CameraScript : MonoBehaviour {
         
         axisY = Mathf.Clamp(axisY, -85f, 85f);
         cameraDistance = Mathf.Clamp(cameraDistance + mouseWheel, 1, 30);
-    
+       
         Quaternion newRotation = Quaternion.Euler(axisY, axisX, 0f);
         transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, lerpRatio);
         
-        transform.position = playerTransform.position - transform.forward * cameraDistance;
-        ObstacleReact();
-    }
-
-    private void ObstacleReact() {
-        bool didHitWall = Physics.Raycast(
+        Vector3 position = playerTransform.position - transform.forward * actualCameraDistance;
+        
+        bool isHittingWall = Physics.Raycast(
             playerTransform.position,
-            transform.position - playerTransform.position,
-            out RaycastHit hit,
-            cameraDistance,
+            position - playerTransform.position,
+            actualCameraDistance,
             obstacle);
         
-        if (didHitWall) {
-            transform.position = hit.point;
+        for (int i = 0; i < 8; i++) {
+            Vector3 direction = Quaternion.Euler(0, 0, i * 45) * Vector3.up;
+            Ray ray = new Ray(transform.position, transform.rotation * direction);
+            if (Physics.Raycast(ray, 0.3f, obstacle) || isHittingWall) {
+                actualCameraDistance = cameraDistance * 0.7f;
+                position = playerTransform.position - transform.forward * actualCameraDistance;
+                break;
+            }
+            actualCameraDistance = cameraDistance;
         }
+        
+        transform.position = position;
     }
-
+    
     // private void OnDrawGizmos() {
     //     Gizmos.color = Color.red;
     //     Vector3 dir = transform.position - playerTransform.position;
